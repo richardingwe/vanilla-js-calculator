de5bea7184b518f3e59a9b2fea0611e03dc08380
richard ingwe <richardingwe5@gmail.com>
2022-01-02 15:34:07 +0100

    added an operator attribute to the buttons

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-alert('linked')
+let displayAnswer = document.querySelector('.answer');
+let displayQuestion = document.querySelector('.question');
+let noButtons = document.querySelectorAll('.noButtons');
+
+init();
+
+noButtons.forEach(button => {
+    button.addEventListener('click', getValue);
+});
+
+
+
+function init() {
+    let answer = 0;
+    let question = 0;
+
+    displayAnswer.innerText = answer;
+    displayQuestion.innerText = question;
+}
+
+function getValue() {
+    const operator = this.getAttribute("operators");
+    console.log(operator);
+}

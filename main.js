6c46fd7a03f1bebfc930de2447aedb94b6e9db8f
richard ingwe <richardingwe5@gmail.com>
2022-01-02 15:48:27 +0100

    added solve function

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let displayAnswer = document.querySelector('.answer');
 let displayQuestion = document.querySelector('.question');
 let noButtons = document.querySelectorAll('.noButtons');
+let solve = document.querySelector('.solve');
+let clearAll = document.querySelector('.clearAll');
+let answer = 0;
+let question = 0;
 
 init();
 
@@ -8,12 +12,13 @@ noButtons.forEach(button => {
     button.addEventListener('click', getValue);
 });
 
+solve.addEventListener('click', solveQuestion);
+
+clearAll.addEventListener('click', clearScreen);
+
 
 
 function init() {
-    let answer = 0;
-    let question = 0;
-
     displayAnswer.innerText = answer;
     displayQuestion.innerText = question;
 }
@@ -21,4 +26,21 @@ function init() {
 function getValue() {
     const operator = this.getAttribute("operators");
     console.log(operator);
+
+    question += operator;
+    console.log(question);
+    displayQuestion.innerText += this.innerText;
+}
+
+function solveQuestion() {
+    answer = eval(question);
+    displayAnswer.innerText = answer;
+}
+
+
+function clearScreen() {
+    answer = 0;
+    question = 0;
+    displayQuestion.innerText = 0;
+    displayAnswer.innerText = 0;
 }
